8d53f8042966600e17648755cb4b6eeebe2d8d3a
Justin <justbriones@gmail.com>
2017-06-25 15:52:52 -0700

    splitWord needs to = randomWord.split, should not push it into splitWord array because it would create an object which conflicts with the toString method when checking if user won

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ var guessesLeft = 9;
 //Functions
 function startGame () {
   randomWord = wordArray[Math.floor(Math.random() * wordArray.length)];
-  splitWord.push(randomWord.split(""));
+  // splitWord.push(randomWord.split(""));
+  splitWord = randomWord.split("");
   numBlanks = randomWord.length;
   //populate blanks and successes with right num of blanksAndSuccess
   for (var i=0; i < numBlanks; i++) {
@@ -29,7 +30,48 @@ function startGame () {
   blanksAndSuccess = [];
 };
 
+function checkLetters(letter) {
+  //check if the letter exists in the word
+  var isLetterInWord = false;
+  for(var i = 0; i<numBlanks; i++) {
+    if(randomWord[i] == letter) {
+      isLetterInWord = true;
+    }
+  }
+  //check where in the word it exists then populate blankAndSuccess array
 
+  if(isLetterInWord) {
+
+
+  for (var i = 0; i<numBlanks; i++) {
+    if(randomWord[i] == letter) {
+      blanksAndSuccess[i] = letter;
+    }
+  }
+}
+
+else {
+  wrongLetters.push(letter);
+  guessesLeft--;
+}
+};
+
+function roundComplete() {
+  //check if user wron
+  if (splitWord.toString() == blanksAndSuccess.toString()) {
+    console.log("you won");
+    startGame();
+  }
+  //check if user lost
+}
 
 //Main process
 startGame();
+
+//register keyclicks
+document.onkeyup = function(event) {
+  var letterGuessed = event.key;
+  checkLetters(letterGuessed);
+  roundComplete();
+
+}
